f9df078e50c0c6dfdbbb936094e22f82f18e2d3d
Kunal Joshi <joshikd7@github.com>
2019-11-25 20:43:56 -0500

    Remove config and new cnn architecture

--- models/cnn.py
+++ models/cnn.py
@@ -1,29 +1,41 @@
-from keras.models import Sequential
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Dropout, Flatten
 
 class ConvNet:
-    def __init__(self, config):
-        self.config = config
+    def __init__(self, optimizer):
+        self.optimizer=optimizer
         self.model = None
         self.build_model()
 
     def build_model(self):
         self.model = Sequential()
-        self.model.add(Conv2D(32, kernel_size=(3, 3),
-                         activation='relu', input_shape=(28, 28, 1)))
-        self.model.add(Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(Conv2D(32, (3, 3), padding='same',
+                        input_shape=x_train.shape[1:]))
+        self.model.add(Activation('relu'))
+        self.model.add(Conv2D(32, (3, 3)))
+        self.model.add(Activation('relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
-        self.model.add(Dropout(0.25))
-        self.model.add(Flatten())
-        self.model.add(Dense(128, activation='relu'))
-        self.model.add(Dropout(0.5))
-        self.model.add(Dense(10, activation='softmax'))
+        
 
-        self.model.compile(
-              loss='sparse_categorical_crossentropy',
-              optimizer=self.config.model.optimizer,
+        self.model.add(Conv2D(64, (3, 3), padding='same'))
+        self.model.add(Activation('relu'))
+        self.model.add(Conv2D(64, (3, 3)))
+        self.model.add(Activation('relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        
+
+        self.model.add(Flatten())
+        self.model.add(Dense(512))
+        self.model.add(Activation('relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(num_classes))
+        self.model.add(Activation('softmax'))
+
+
+# Let's train the model 
+         self.model.compile(loss='categorical_crossentropy',
+              optimizer=self.optimizer,
               metrics=['accuracy'])
-    
+
+
     def save(self, checkpoint_path):
         if self.model is None:
             raise Exception("You have to build the model first.")
--- models/cnn_dropout.py
+++ models/cnn_dropout.py
@@ -1,29 +1,42 @@
-from keras.models import Sequential
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Dropout, Flatten
 
 class ConvNetDropout:
-    def __init__(self, config):
-        self.config = config
+    def __init__(self, optimizer, num_classes):
+        self.optimizer = optimizer
         self.model = None
+        self.num_classes = num_classes
         self.build_model()
 
     def build_model(self):
         self.model = Sequential()
-        self.model.add(Conv2D(32, kernel_size=(3, 3),
-                         activation='relu', input_shape=(28, 28, 1)))
-        self.model.add(Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(Conv2D(32, (3, 3), padding='same',
+                        input_shape=x_train.shape[1:]))
+        self.model.add(Activation('relu'))
+        self.model.add(Conv2D(32, (3, 3)))
+        self.model.add(Activation('relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
         self.model.add(Dropout(0.25))
-        self.model.add(Flatten())
-        self.model.add(Dense(128, activation='relu'))
-        self.model.add(Dropout(0.5))
-        self.model.add(Dense(10, activation='softmax'))
 
-        self.model.compile(
-              loss='sparse_categorical_crossentropy',
-              optimizer=self.config.model.optimizer,
+        self.model.add(Conv2D(64, (3, 3), padding='same'))
+        self.model.add(Activation('relu'))
+        self.model.add(Conv2D(64, (3, 3)))
+        self.model.add(Activation('relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(512))
+        self.model.add(Activation('relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(self.num_classes))
+        self.model.add(Activation('softmax'))
+
+
+# Let's train the model 
+        self.model.compile(loss='categorical_crossentropy',
+              optimizer=self.optimizer,
               metrics=['accuracy'])
-    
+
+
     def save(self, checkpoint_path):
         if self.model is None:
             raise Exception("You have to build the model first.")
